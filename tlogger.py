76bf8224200fee8a11ca23fc72e648b76a1f0476
FrancisChung <francis.c.chung@gmail.com>
2021-12-11 16:12:44 +1100

    Basic implementation of a Python Logger

---
+++ tlogger.py
@@ -0,0 +1,99 @@
+import logging
+import os, sys
+
+from functools import wraps
+
+# Be sure this is set to False/Commented out before you deploy to AWS!
+# COLOUR_FORMATTER = True
+
+CRITICAL = 50
+FATAL = CRITICAL
+ERROR = 40
+WARNING = 30
+WARN = WARNING
+INFO = 20
+DEBUG = 10
+NOTSET = 0
+
+
+def newobj(method):
+    @wraps(method)
+    # Well, newobj can be decorated with function, but we will cover the case
+    # where it decorated with method
+    def inner(self, *args, **kwargs):
+        obj = self.__class__.__new__(self.__class__)
+        obj.__dict__ = self.__dict__.copy()
+        method(obj, *args, **kwargs)
+        return obj
+
+    return inner
+
+
+class TLogger():
+    """
+    T(EASE)Logger : Wrapper class for Standard Python logger with some presets for TEASE,
+                    to help down on code duplication and enforce project wide standards
+
+    Parameters:
+        name        : Name of the Logger
+        infoLevel   : logging level of the Logger (e.g. logging.DEBUG/INFO/WARNING/ERROR)
+        fileHandler : string file path to the file handler log file (e.g. /logs/xyz.log)
+    """
+
+    def __init__(self, name: str, infoLevel=logging.INFO, fileHandler=None):
+        try:
+            if name is None:
+                raise ValueError("Name argument not specified")
+
+            logformat = '%(asctime)s %(levelname)s [%(name)s %(funcName)s] %(message)s'
+            self.logformat = logformat
+            self.name = name.upper()
+            self.logger = logging.getLogger(self.name)
+            self.logger.setLevel(infoLevel)
+
+            self.add_consolehandler(infoLevel, logformat)
+
+        except Exception as e:
+            if self.logger:
+                self.logger.error(str(e))
+
+    def error(self, message):
+        self.logger.error(message, stack_info=True, exc_info=True)
+
+    def info(self, message):
+        self.logger.info(message)
+
+    def warning(self, message):
+        self.logger.warning(message)
+
+    def debug(self, message):
+        self.logger.debug(message)
+
+    def critical(self, message):
+        self.logger.critical(message, stack_info=True, exc_info=True)
+
+    def setLevel(self, infoLevel):
+        # Python's standard Logging has lots of design issues, especially with its API and expected behaviours
+        # To dynamically reset the loglevel, you need to also change the parent levels as well as all handlers!
+        self.logger.parent.setLevel(infoLevel)
+        for handler in self.logger.parent.handlers:
+            handler.setLevel(infoLevel)
+
+        self.logger.setLevel(infoLevel)
+        for handler in self.logger.handlers:
+            handler.setLevel(infoLevel)
+
+        return self.logger.level
+
+    @newobj
+    def add_consolehandler(self, infoLevel=logging.INFO,
+                           logformat='%(asctime)s %(levelname)s [%(name)s %(funcName)s] %(message)s'):
+        sh = logging.StreamHandler()
+        sh.setLevel(infoLevel)
+
+        formatter = logging.Formatter(logformat)
+        sh.setFormatter(formatter)
+        self.logger.addHandler(sh)
+
+
+
